96d7df81ecb410d4f2301844ec5540a9a932ce10
Huro Dmytro <31777360+Kalinia@users.noreply.github.com>
2020-10-27 20:10:54 +0200

    Interface (#10)
    * 'UI'
    
    * Зроблена загрузка файлів, проте тут назва папки загрузки генерується рандомно
    
    Co-authored-by: Kalinia <kaliniaguro@mail.com>

---
+++ app/forms/new_schedule.py
@@ -0,0 +1,17 @@
+from flask_wtf import Form
+from wtforms import SubmitField, ValidationError, StringField
+from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileRequired
+
+class New_schedule_form(Form):
+    email = StringField(validators=[InputRequired()])
+    name = StringField(validators=[InputRequired()])
+    teachers = FileField(validators=[FileRequired()])
+    subjects = FileField(validators=[FileRequired()])
+    groups = FileField(validators=[FileRequired()])
+    faculty = FileField(validators=[FileRequired()])
+    departments = FileField(validators=[FileRequired()])
+    load_list = FileField(validators=[FileRequired()])
+    teacher_emails = FileField(validators=[FileRequired()])
+    student_emails = FileField(validators=[FileRequired()])
+    submit = SubmitField("Підтвердити")
